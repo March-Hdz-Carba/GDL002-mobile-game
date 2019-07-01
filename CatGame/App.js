f8095dc8d5a3317ce405610941425fae491013e9
unknown <marcela.hdez.1801@gmail.com>
2019-07-01 18:46:53 -0500

    Add alert for each way to win

--- CatGame/App.js
+++ CatGame/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, TouchableOpacity, Alert, Button, ImageBackground, Image } from 'react-native';
 //import { MaterialCommunityIcons as Icon } from "react-native-vector-icons";
-import { FontAwesome as IconNave } from "react-native-vector-icons";
+//import { FontAwesome as IconNave } from "react-native-vector-icons";
 import Header from "./components/Header";
 
 export default class App extends Component {
@@ -21,19 +21,26 @@ export default class App extends Component {
     this.initializeGame();
   }
 
-
-  initializeGame (){
+  initializeGame = () => {
     this.setState({
-      ...this.state,
-      gameState:
-      [
-        [0, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0]  
-      ],
-      currentPlayer: 1,
-    })
-  }
+      gameState: [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+      currentPlayer: 1
+    });
+  };
+
+
+  // initializeGame (){
+  //   this.setState({
+  //     ...this.state,
+  //     gameState:
+  //     [
+  //       [0, 0, 0],
+  //       [0, 0, 0],
+  //       [0, 0, 0]  
+  //     ],
+  //     currentPlayer: 1,
+  //   })
+  // }
 
   //Return 1 if Player 1 won, -1 if player 2 won, or a 0 if no one has won.
   getWinner = () => {
@@ -42,44 +49,77 @@ export default class App extends Component {
     let sum;
 
     //check rows
-    for (let i = 0; i < numTile; i++)
+    for (let i = 0; i < numTile; i++){
       sum = arr[i][0] + arr[i][1] + arr[i][2];
-      if (sum === 3) {return 1 }
-      else if (sum === -3) {return -1 }
+      if (sum == 3) {
+        return [1, 1];
+       }
+        else if (sum == -3) {
+          return [-1, 1];
+         }
+        }
 
     //check columns
-    for (let i = 0; i < numTile; i++)
+    for (let i = 0; i < numTile; i++){
       sum = arr[0][i] + arr[1][i] + arr[2][i];
-      if (sum === 3) {return 1 }
-      else if (sum === -3) {return -1 }
+      if (sum == 3) {
+        return [1, 0];
+       }
+        else if (sum == -3) {
+          return [-1, 0];
+         }
+        }
       
     //check diagonal
       sum = arr[0][0] + arr[1][1] + arr[2][2];
-      if (sum === 3) {return 1 }
-      else if (sum === -3) {return -1 }
+      if (sum == 3) {
+        return [1, 2];
+       }
+      else if (sum == -3) {
+        return [-1, 2];
+       }
       
       sum = arr[2][0] + arr[1][1] + arr[0][2];
-      if (sum === 3) {return 1 }
-      else if (sum === -3) {return -1 }
+      if (sum == 3) {
+        return [1, 3];
+       }
+      else if (sum == -3) {
+        return [-1, 3];
+       }
 
     //No winners
-    let draw = 0;
-    for (let i=0; i< numTile; i++){
-      for (let j=0; j< numTile; j++){
-        if (this.state.gameState[i][j] === 0){
-          draw++
+    // let draw = 0;
+    // for (let i=0; i< numTile; i++){
+    //   for (let j=0; j< numTile; j++){
+    //     if (this.state.gameState[i][j] === 0){
+    //       draw++
+    //     }
+    //   }
+    // }
+    // if (draw === 0){
+    // return 0
+    // }  
+    let allOver = 0;
+    for (let i = 0; i < numTile; i++) {
+      for (let j = 0; j < numTile; j++) {
+        if (arr[i][j] == 1 || arr[i][j] == -1) {
+          allOver += 1;
+          if (allOver == 9) {
+            return [2, 4];
+          }
         }
       }
     }
-    if (draw === 0){
-    return 0
-    }  
+
+    return 0;
   }
 
   onTilePress = (row, col) => {
     // Dont allow tile to change...
     let value = this.state.gameState[row][col];
-    if (value != 0) {return; }
+    if (value != 0) {
+      return; 
+    }
 
     //Grab current player...
     let currentPlayer = this.state.currentPlayer;
@@ -91,22 +131,76 @@ export default class App extends Component {
 
     //Switch to other player...
     let nextPlayer = (currentPlayer == 1) ? -1 : 1;
-    this.setState ({currentPlayer: nextPlayer,});
+    this.setState ({currentPlayer: nextPlayer});
+  //}
 
-    //Check for winners
+    //afterTurn(){
     let winner = this.getWinner();
-    if (winner === 1) { 
-      Alert.alert("Player 1 is the winner");
-      this.initializeGame();
-    } else if (winner === -1) {
-      Alert.alert("Player 2 is the winner");
-      this.initializeGame();
-    }
-    else if (winner === 0){
-      Alert.alert("Empate, Intetalo de nuevo");
-      this.initializeGame();
-    }
-  }
+    if (winner[0] == 1) {
+      switch (winner [1]) {
+        case 0: {Alert.alert("Jugador 1 es el Ganador!!!");
+        this.initializeGame();} 
+          
+          break;
+      
+        case 1: {Alert.alert("Jugador 1 es el Ganador!!!");
+        this.initializeGame();} 
+          
+          break;
+        
+        case 2: {Alert.alert("Jugador 1 es el Ganador!!!");
+        this.initializeGame();} 
+          
+          break;
+
+        case 3: {Alert.alert("Jugador 1 es el Ganador!!!");
+        this.initializeGame();} 
+          
+          break;
+      }
+    } 
+        else if (winner[0] == -1) {
+          switch (winner [1]) {
+            case 0: {Alert.alert("Jugador 2 es el Ganador!!!");
+            this.initializeGame();} 
+              
+              break;
+          
+            case 1: {Alert.alert("Jugador 2 es el Ganador!!!");
+            this.initializeGame();} 
+              
+              break;
+            
+            case 2: {Alert.alert("Jugador 2 es el Ganador!!!");
+            this.initializeGame();} 
+              
+              break;
+    
+            case 3: {Alert.alert("Jugador 2 es el Ganador!!!");
+            this.initializeGame();} 
+              
+              break;
+          }
+        }
+          else if (winner[0]== 2){
+            if (winner[1] == 4){
+              Alert.alert("Empate, Intetalo de nuevo");
+              this.initializeGame();
+              }
+            }
+          };
+        
+    //   Alert.alert("Player 1 is the winner");
+    //   this.initializeGame();
+    // } else if (winner === -1) {
+    //   Alert.alert("Player 2 is the winner");
+    //   this.initializeGame();
+    // }
+    // else if (winner === 0){
+    //   Alert.alert("Empate, Intetalo de nuevo");
+    //   this.initializeGame();
+    // }
+  
 
   renderIcon = (row, col) => {
     let value = this.state.gameState[row][col];
@@ -116,7 +210,7 @@ export default class App extends Component {
       case -1: return <Image source={require("../CatGame/assets/ovni.png")} style={styles.tileO} />;
       default: return <View />;
     }
-  }
+  };
 
 render(){
   return (
